dcc3df157c8ebd77111c28c26edcbd5d4b6f8797
CKY <awa@outlook.ie>
2023-01-05 12:36:14 +0800

    Update index.js

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,7 +1,9 @@
 import DefaultTheme from 'vitepress/theme'
+import theme from 'vitepress/theme'
 import Comment from "./comment.vue";
 export default {
     ...DefaultTheme,
+    ...theme,
     enhanceApp({ app, router, siteData }) {
         app.component('Comment', Comment)
     }
