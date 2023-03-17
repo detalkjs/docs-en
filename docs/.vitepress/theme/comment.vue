c3a3af32f7235f6159fdc516ef3f20321f99efd9
CKY <awa@outlook.ie>
2023-03-17 18:44:12 +0800

    Update comment.vue

--- docs/.vitepress/theme/comment.vue
+++ docs/.vitepress/theme/comment.vue
@@ -29,11 +29,24 @@ export default {
         //     }
         // });
         setTimeout(() => {
+            function blobToBase64(blob) {
+                return new Promise((resolve, reject) => {
+                    const fileReader = new FileReader();
+                    fileReader.onload = (e) => {
+                    resolve(e.target.result);
+                    };
+                    // readAsDataURL
+                    fileReader.readAsDataURL(blob);
+                    fileReader.onerror = () => {
+                    reject(new Error('blobToBase64 error'));
+                    };
+                });
+            }
             detalk.init({
                 // 挂载点 (CSS 选择器)
                 el: "#detalk", // <div id="app"></div>
                 // 后端地址
-                url: "https://detalk.deta.dev/",
+                url: "https://detalkjs-server.cky.im/",
                 // 当前页面的路径
                 path: window.location.pathname,
                 // 是否开启实时预览
@@ -53,6 +66,19 @@ export default {
                 },
                 "recaptchaSiteKey": "6LefUdojAAAAAHe0G0p0MaW5U93AnH_G5QViDl92",
                 "recaptchaLoaded": false,
+                pasteImage: async (blob) => {
+                    let base64 = (await blobToBase64(blob)).split(',')[1];
+                    var suffix = blob.type.split("/")[1];
+                    var imgUrl = await fetch("https://p1.ck3.tech/s/", {
+                        method: "POST",
+                        body: JSON.stringify({
+                            content: base64,
+                            suffix,
+                        })
+                    }).then(resp => resp.json());
+
+                    return `https://p1.ck3.tech${imgUrl.url}`;
+                }
             });
         }, 1000);
     },
